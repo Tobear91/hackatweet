430f38eca77d435dd39ac50e3581fa67431826bb
Pierre BEJIAN <pierre.bejian@gmail.com>
2025-07-17 15:22:41 +0200

    Routes signup et signin

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,76 @@
 var express = require('express');
 var router = express.Router();
 
+const User = require('../models/users');
+const { checkBody } = require('../modules/checkBody');
+const uid2 = require('uid2');
+const bcrypt = require('bcrypt');
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+
+// Route POST pour inscription
+router.post('/signup', (req, res) => {
+  if (!checkBody(req.body, ['name', 'email', 'pseudo', 'password'])) {
+    res.json({ result: false, error: 'Missing or empty fields' });
+    return;
+  }
+
+  // Check if the user has not already been registered
+  User.findOne({ email: req.body.email }).then(data => {
+    if (data === null) {
+      const hash = bcrypt.hashSync(req.body.password, 10);
+
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        pseudo: req.body.pseudo,
+        password: hash,
+        token: uid2(32),
+        photo: "",
+      });
+
+      newUser.save().then(newDoc => {
+        console.log("New user:", newDoc.email)
+        res.json({ result: true, token: newDoc.token });
+      });
+    } else {
+      // User already exists in database
+      res.json({ result: false, error: 'User already exists' });
+    }
+  });
+});
+
+
+
+// Route POST pour connexion
+router.post('/signin', (req, res) => {
+  if (!checkBody(req.body, ['email', 'password'])) {
+    res.json({ result: false, error: 'Missing or empty fields' });
+    return;
+  }
+
+  User.findOne({ email: req.body.email }).then(data => {
+    if (data && bcrypt.compareSync(req.body.password, data.password)) {
+      console.log("User connected:", data.email)
+      res.json({ result: true, token: data.token });
+    } else {
+      res.json({ result: false, error: 'User not found or wrong password' });
+    }
+  });
+});
+
+
+
+
+
+
+
+
+
+
 
 module.exports = router;
