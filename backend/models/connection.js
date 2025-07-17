edd803101833e18d96040905dd189e7b479bf67e
Yann LC <lcyann85@gmail.com>
2025-07-17 12:10:17 +0200

    Creation fichiers connection.js / Connexion string

---
+++ backend/models/connection.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+const mongoose = require('mongoose');
+
+const CONNECTION_STRING = process.env.CONNECTION_STRING;
+
+mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
+    .then(() => console.log('Database connected'))
+
+    .catch(error => console.error(error));
