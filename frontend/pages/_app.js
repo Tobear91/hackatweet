705760c5acd3a7e8f873d5f33311effecabe038f
Camille HUrtaud <camille.hurtaud@gmail.com>
2025-07-18 09:14:45 +0200

    Ajout de redux et redux store

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,14 +1,34 @@
 import "../styles/core/globals.scss";
 import Head from "next/head";
 
+import { Provider } from "react-redux";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import users from "../reducers/users";
+
+import { persistStore, persistReducer } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
+import storage from "redux-persist/lib/storage";
+
+const reducers = combineReducers({ users });
+const persistConfig = { key: "hackatweet", storage };
+
+const store = configureStore({
+  reducer: persistReducer(persistConfig, reducers),
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }),
+});
+
+const persistor = persistStore(store);
+
 function App({ Component, pageProps }) {
   return (
-    <>
-      <Head>
-        <title>Hackatweet</title>
-      </Head>
-      <Component {...pageProps} />
-    </>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <Head>
+          <title>Hackatweet</title>
+        </Head>
+        <Component {...pageProps} />
+      </PersistGate>
+    </Provider>
   );
 }
 
